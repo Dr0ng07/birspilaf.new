34ff330c367ecb1fc0377d377dad7c44cccb1b70
Dr0ng07 <drongo207777@gmail.com>
2025-10-29 16:21:42 -0700

    header

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-fetch('index.html')
+fetch('chunks.html')
 .then(r => r.text())
 .then(html =>{
+    const tempDiv = document.createElement ('div');
+    tempDiv.innerHTML = html;
+    const meni = tempDiv.querySelector ('#meni');
+
     document.getElementById ('meni'). innerHTML = html;
 })
 .catch(err => console.error('Миша все хуйня:' , err));

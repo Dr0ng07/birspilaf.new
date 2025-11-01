b7be617062763660608a0250c4e546c2ac4c7fea
Dr0ng07 <drongo207777@gmail.com>
2025-10-31 18:56:50 -0700

    js2

--- script.js
+++ script.js
@@ -7,31 +7,9 @@ fetch('chunks.html')
 
     document.getElementById ('meni'). innerHTML = html;
 })
-.catch(err => console.error('Миша все хуйня:' , err));
 
-document.addEventListener("DOMContentLoaded" , function(){
-const form = document.getElementById("reviewForm");
-const status = document.getElementById("status");
- if (!form) return;
-    form.addEventListener ("submit"), async function (event) {
-     event.preventDefault();
-     const formData = new FormData (form);
-     status.textContent= "⌛️";
-     try{ 
-        const responce = await fetch("https://httpbin.org/post"{
-        method: "POST",
-        body: formData
-        });
-     if (responce.ok){
-        status.textContent = "❤️";
-        form.reser();
-     } else
-        status.textContent = "😭";
-     }
-    
-     catch (error){
-        status.textContent = "Xxxx" + error.message;
-     }
- };
+
+
+  
 
   
